7cc3f7cf3abe9b866391e76cf008a21b5d98c84f
HusrevAkbas <husrev.akbas@gmail.com>
2025-11-26 18:18:01 +0100

    refactor: secrets  filenames changed according to where they used

--- srcs/requirements/wordpress/tools/starter.sh
+++ srcs/requirements/wordpress/tools/starter.sh
@@ -24,30 +24,30 @@ if [[ ! -f $CONFIG_FILE ]] || [[ ! -s $CONFIG_FILE ]]; then
 	
 	# set wp-config.php
 	echo "<?php" > $CONFIG_FILE
-	echo "define( 'DB_NAME', '$(cat /run/secrets/db_dbname)' );" >> $CONFIG_FILE
-	echo "define( 'DB_USER', '$(cat /run/secrets/db_adminname)' );" >> $CONFIG_FILE
-	echo "define( 'DB_PASSWORD', '$(cat /run/secrets/db_adminpass)' );" >> $CONFIG_FILE
+	echo "define( 'DB_NAME', '$(cat /run/secrets/wp_dbname)' );" >> $CONFIG_FILE
+	echo "define( 'DB_USER', '$(cat /run/secrets/db_username)' );" >> $CONFIG_FILE
+	echo "define( 'DB_PASSWORD', '$(cat /run/secrets/db_password)' );" >> $CONFIG_FILE
 	echo "define( 'DB_HOST', '$WORDPRESS_DB_HOST' );" >> $CONFIG_FILE
 	echo "define( 'DB_CHARSET', 'utf8mb4' );" >> $CONFIG_FILE
 	echo "define( 'DB_COLLATE', '' );" >> $CONFIG_FILE
 	echo "define( 'WP_HOME', 'https://huakbas.42.fr');" >> $CONFIG_FILE
 	echo "define( 'WP_SITEURL', 'https://huakbas.42.fr');" >> $CONFIG_FILE
 	echo "$SALT" >> $CONFIG_FILE
-	echo '$table_prefix'" = 'wp_';" >> $CONFIG_FILE
+	echo '$table_prefix'" = 'inception_';" >> $CONFIG_FILE
 	echo "if ( ! defined( 'ABSPATH' ) ) { define( 'ABSPATH', __DIR__ . '/' ); }" >> $CONFIG_FILE
 	echo "require_once ABSPATH . 'wp-settings.php';" >> $CONFIG_FILE
 
 	# set variables in install.php
 	echo $INSTALL_FILE > fileName
 	sed -i "s/titleHere/Good One/" $INSTALL_FILE
-	sed -i "s/adminnameHere/$(cat /run/secrets/db_adminname)/" $INSTALL_FILE
-	sed -i "s/adminpassHere/$(cat /run/secrets/db_adminpass)/" $INSTALL_FILE
+	sed -i "s/adminnameHere/$(cat /run/secrets/wp_adminname)/" $INSTALL_FILE
+	sed -i "s/adminpassHere/$(cat /run/secrets/wp_adminpass)/" $INSTALL_FILE
 	sed -i "s/emailHere/huakbas@42vienna.com/" $INSTALL_FILE
 
 	# add other user
 	echo "function other_account(){" >> temp
-	echo '$user = '"'$(cat /run/secrets/db_username)';" >> temp
-	echo '$pass = '"'$(cat /run/secrets/db_userpass)';" >> temp
+	echo '$user = '"'$(cat /run/secrets/wp_username)';" >> temp
+	echo '$pass = '"'$(cat /run/secrets/wp_userpass)';" >> temp
 	echo '$email'" = 'email@domain.com';" >> temp
 	echo 'if ( !username_exists( $user ) && !email_exists( $email ) ) {' >> temp
 	echo '$user_id = wp_create_user( $user, $pass, $email );' >> temp
