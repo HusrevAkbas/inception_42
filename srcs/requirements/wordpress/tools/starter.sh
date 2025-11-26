9bccc02ce11bf8f299f19d8507486bd4b907460f
HusrevAkbas <husrev.akbas@gmail.com>
2025-11-26 15:47:06 +0100

    feat: second user added through functions.php

--- srcs/requirements/wordpress/tools/starter.sh
+++ srcs/requirements/wordpress/tools/starter.sh
@@ -4,12 +4,7 @@ echo "This is WORDPRESS script"
 
 CONFIG_FILE=/var/www/html/wp-config.php
 INSTALL_FILE=/var/www/html/wp-admin/install.php
-
-echo "Filename: $INSTALL_FILE"
-
-#echo "config file: $CONFIG_FILE"
-#echo "db name is $(cat /run/secrets/db_dbname)"
-#ECHO "HOST: $WORDPRESS_DB_HOST"
+FUNCS_FILE=/var/www/html/wp-content/themes/twentytwentyfive/functions.php
 
 set -e
 
@@ -23,8 +18,11 @@ set -e
 #trap "signal_terminate_trap" SIGTERM
 
 if [[ ! -f $CONFIG_FILE ]] || [[ ! -s $CONFIG_FILE ]]; then
+	
 	# download authentication keys
 	SALT=$(curl -L https://api.wordpress.org/secret-key/1.1/salt/)
+	
+	# set wp-config.php
 	echo "<?php" > $CONFIG_FILE
 	echo "define( 'DB_NAME', '$(cat /run/secrets/db_dbname)' );" >> $CONFIG_FILE
 	echo "define( 'DB_USER', '$(cat /run/secrets/db_adminname)' );" >> $CONFIG_FILE
@@ -38,17 +36,28 @@ if [[ ! -f $CONFIG_FILE ]] || [[ ! -s $CONFIG_FILE ]]; then
 	echo '$table_prefix'" = 'wp_';" >> $CONFIG_FILE
 	echo "if ( ! defined( 'ABSPATH' ) ) { define( 'ABSPATH', __DIR__ . '/' ); }" >> $CONFIG_FILE
 	echo "require_once ABSPATH . 'wp-settings.php';" >> $CONFIG_FILE
+
 	# set variables in install.php
 	echo $INSTALL_FILE > fileName
 	sed -i "s/titleHere/Good One/" $INSTALL_FILE
 	sed -i "s/adminnameHere/$(cat /run/secrets/db_adminname)/" $INSTALL_FILE
 	sed -i "s/adminpassHere/$(cat /run/secrets/db_adminpass)/" $INSTALL_FILE
 	sed -i "s/emailHere/huakbas@42vienna.com/" $INSTALL_FILE
-	cat $INSTALL_FILE
-#	echo "wp-config.php NOT FOUND"
+
+	# add other user
+	echo "function other_account(){" >> temp
+	echo '$user = '"'$(cat /run/secrets/db_username)';" >> temp
+	echo '$pass = '"'$(cat /run/secrets/db_userpass)';" >> temp
+	echo '$email'" = 'email@domain.com';" >> temp
+	echo 'if ( !username_exists( $user ) && !email_exists( $email ) ) {' >> temp
+	echo '$user_id = wp_create_user( $user, $pass, $email );' >> temp
+	echo '$user = new WP_User( $user_id );' >> temp
+	echo '$user'"->set_role( 'author' );" >> temp
+	echo "} }" >> temp
+	echo "add_action('init','other_account');">> temp
+	cat temp >> $FUNCS_FILE
 else
 	echo "wp-config.php IS ALREADY EXIST $CONFIG_FILE"
-	cat $CONFIG_FILE
 fi
 
 # -F: keeps php-fpm in foreground, -R force porgram run as root
